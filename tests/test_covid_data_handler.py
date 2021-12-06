b613b30c6d85731bc694c9b263fa8c525c6aedec
tom-newbold <thomas_newbold@outlook.com>
2021-12-06 14:37:46 +0000

    updated

--- tests/test_covid_data_handler.py
+++ tests/test_covid_data_handler.py
@@ -4,13 +4,13 @@ from covid_data_handler import covid_API_request
 from covid_data_handler import schedule_covid_updates
 
 def test_parse_csv_data():
-    data = parse_csv_data('tests\\nation_2021-10-28.csv')
+    data = parse_csv_data('nation_2021-10-28.csv')
     assert len(data) == 639
 
 def test_process_covid_csv_data():
     last7days_cases , current_hospital_cases , total_deaths = \
         process_covid_csv_data ( parse_csv_data (
-            'tests\\nation_2021-10-28.csv' ) )
+            'nation_2021-10-28.csv' ) )
     assert last7days_cases == 240_299
     assert current_hospital_cases == 7_019
     assert total_deaths == 141_544
